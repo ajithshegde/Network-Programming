b51c98c7e5fe8f27f40a8cf3b9622a2a009f817f
Ajith Hegde <ajithshridhar.hegde@stonybrook.edu>
2015-09-27 02:19:19 -0400

    del

--- tcpechotimecli.c
+++ tcpechotimecli.c
@@ -7,12 +7,12 @@ main(int argc, char **argv)
 	struct in_addr addr_4;
 	char choice;
 	char str[INET_ADDRSTRLEN];
-	//	const char *addr1;
+
 
 	if (argc != 2)
 		err_quit("usage: a.out <host_name>");
 	if(inet_pton(AF_INET, argv[1], &addr_4)<= 0){
-		//	err_quit("Invalid IP");
+		
 		if ((ha = gethostbyname(argv[1])) == NULL) {  
 			err_quit("Invalid host name or IP address");
 			return 2;
